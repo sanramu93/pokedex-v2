f4bdd35d7a33e3c23c531ac19925d5cd77697642
Santiago <sanramu93@gmail.com>
2023-02-02 20:34:12 -0500

    Create layout contianer component -- Add Typo variables

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 
 @NgModule({
   declarations: [
-    SearchBarComponent
+    SearchBarComponent,
+    LayoutComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SearchBarComponent
+    SearchBarComponent,
+    LayoutComponent
   ]
 })
 export class SharedModule { }
