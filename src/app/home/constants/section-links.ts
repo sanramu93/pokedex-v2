8215afdad2d88c603f2cad5062910c1c0765adf6
Santiago <sanramu93@gmail.com>
2023-02-11 11:42:42 -0500

    Fix Home layout -- Add Section links

---
+++ src/app/home/constants/section-links.ts
@@ -0,0 +1,22 @@
+export const SECTION_LINKS = [
+  {
+    text: 'Types',
+    color: '#A7DB8D',
+    iconPath: '../../assets/img/icons/pokeball-icon.svg'
+  },
+  {
+    text: 'Locations',
+    color:'#F5AC78',
+    iconPath: '../../assets/img/icons/location-icon.svg'
+  },
+  {
+    text: 'Moves and Abilities',
+    color: '#9DB7F5',
+    iconPath: '../../assets/img/icons/star-icon.svg'
+  },
+  {
+    text: 'Favorites',
+    color: '#FF5959',
+    iconPath: '../../assets/img/icons/heart-icon.svg'
+  }
+]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SECTION_LINKS } from './constants/section-links';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  public menuItems = ['Types', 'Locations', ' Move and Abilities', 'Favorites'];
+  public sectionLinks = [...SECTION_LINKS];
 
   constructor() { }
 
