dbb90ec2cb638a797bb6a5d495f93b79fc3d2a34
Santiago <sanramu93@gmail.com>
2023-03-05 10:42:13 -0500

    Add gradient bg according to type

--- src/shared/components/pokemon-detail/pokemon-detail.component.ts
+++ src/shared/components/pokemon-detail/pokemon-detail.component.ts
@@ -10,6 +10,8 @@ import { PokemonDataService } from 'src/shared/services/pokemon-data.service';
 export class PokemonDetailComponent implements OnInit {
 
   public id: number;
+  public typeParam: string;
+  public types: string[];
   public pokemon: any;
 
   constructor(
@@ -19,15 +21,31 @@ export class PokemonDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    this.typeParam = this.route.snapshot.params['type'];
     this.getPokemonById(this.id);
+  }
+
+  public formatId(id: number) {
+    return String(id).padStart(4, '0');
+  }
+
+  public getBgColorClass(types: string[]) {
+    return types?.includes(this.typeParam) ? 
+            `bg-${this.typeParam}-grad` : 
+            `bg-${types?.[0]}-grad`;
   }
 
   private getPokemonById(id: number) {
     this.dataService.getPokemonById(id)
     .subscribe({
-      next: pokemon => this.pokemon = pokemon,
+      next: pokemon => {
+        this.pokemon = pokemon;
+        this.types = this.pokemon.types.map((type:any) => type.type.name);
+      },
       error: err => console.log(err)
     })
   }
+
+
 
 }
